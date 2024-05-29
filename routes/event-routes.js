96505a5d494ce06410d0f1fdf0020d29065b1753
IngMohamed <mohamedbouchaniata123@gmail.com>
2024-05-29 15:48:40 +0100

    commit 3

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -1,10 +1,18 @@
 const express = require("express")
 const router = express.Router()
 
+// router to home events
 router.get('/', (req,res)=>{
 
     res.render('event/indexv')
 })
+
+// show signal evenet
+router.get('/:id', (req,res)=>{
+
+    res.render('event/show')
+})
+
 
 
 module.exports = router
